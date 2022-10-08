9db5f18de77889b1c19dfa455977bba6ff95f675
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2022-10-08 23:19:33 +0700

    Update video URLs.

--- components/_layout/layout.jsx
+++ components/_layout/layout.jsx
@@ -24,7 +24,8 @@ export const Layout = ({ bgVideo, slideFn }) => {
       <div className={bg}>
         <video
           id='video-main'
-          src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1662286408/emerging/ferns_exk1af.mp4`}
+          // src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1662286408/emerging/ferns_exk1af.mp4`}
+          src={`https://res.cloudinary.com/datacom-cabling/video/upload/v1665245822/es/ferns_rh2ujp.mp4`}
           autoPlay
           loop
           muted
@@ -32,8 +33,8 @@ export const Layout = ({ bgVideo, slideFn }) => {
         />
         <video
           id='video-about'
-          src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1663493648/emerging/about_gmtfyh.mp4`}
-          // src={`https://res.cloudinary.com/jameswalker-work/video/upload/c_crop,g_west,x_200/v1663493648/emerging/about_gmtfyh.mp4`}
+          // src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1663493648/emerging/about_gmtfyh.mp4`}
+          src={`https://res.cloudinary.com/datacom-cabling/video/upload/v1665245844/es/Es_About-Vid-1--no-audio_sa0p2b.mp4`}
           autoPlay
           loop
           muted
@@ -41,7 +42,8 @@ export const Layout = ({ bgVideo, slideFn }) => {
         />
         <video
           id='video-end'
-          src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1663170955/emerging/mountains-2_rjf9dr.mp4`}
+          // src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1663170955/emerging/mountains-2_rjf9dr.mp4`}
+          src={`https://res.cloudinary.com/datacom-cabling/video/upload/v1665245818/es/mountains-2_v1kjhi.mp4`}
           autoPlay
           loop
           muted
