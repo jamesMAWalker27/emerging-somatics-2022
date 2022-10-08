917f72207b257696cc92bffb88f5c6188600b6a6
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2022-10-08 21:15:41 +0700

    Animate in hero text.

--- components/hero/index.jsx
+++ components/hero/index.jsx
@@ -1,3 +1,4 @@
+import gsap from 'gsap'
 import React, { useEffect, useState } from 'react'
 
 import { Wordmark } from '../_svg/wordmark'
@@ -19,10 +20,19 @@ export const Hero = ({ slideFn }) => {
     slideFn.current(e)
   }
 
+  const fadeInFonts = () => {
+    gsap.fromTo(
+      '#hero-text',
+      { opacity: 0 },
+      { opacity: 1, delay: 0.25, duration: 1 }
+    )
+  }
+
   // fade content after fonts loaded
   useEffect(() => {
     document.fonts.ready.then(() => {
-      setFontsLoaded(true)
+      // setFontsLoaded(true)
+      fadeInFonts()
     })
   })
 
@@ -31,19 +41,20 @@ export const Hero = ({ slideFn }) => {
       <h1 className={companyTitle}>
         <Wordmark />
       </h1>
-      <div className={centerContent}>
-        {fontsLoaded && (
-          <>
-            <h2 className={header}>Discover Embodied Leadership.</h2>
-            <p className={subheader}>
-              Skillful action, pragmatic wisdom, and grounded compassion.
-            </p>
-            <button className={btnScroll} data-action={'slide-link'} id='1' onClick={handleScrollNextSection}>
-              scroll to proceed →
-            </button>
-          </>
-        )}
-      </div>
+        <div className={centerContent} id='hero-text'>
+          <h2 className={header}>Discover Embodied Leadership.</h2>
+          <p className={subheader}>
+            Skillful action, pragmatic wisdom, and grounded compassion.
+          </p>
+          <button
+            className={btnScroll}
+            data-action={'slide-link'}
+            id='1'
+            onClick={handleScrollNextSection}
+          >
+            scroll to proceed →
+          </button>
+        </div>
     </div>
   )
 }
