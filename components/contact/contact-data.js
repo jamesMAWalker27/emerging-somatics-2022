20fba4dd96d6e18d656ecff292fffa9549b45342
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-03-07 02:49:54 +0700

    Add new contact page content; adjust styling.

--- components/contact/contact-data.js
+++ components/contact/contact-data.js
@@ -2,29 +2,35 @@ import { MapsIcon, TwitterIcon, LinkedInIcon } from "../_svg/business-icons";
 
 export const CONTACT_CONTENT = {
   blurbContent: {
-    header: <>
-      Let's have a
-      <br />
-      Conversation.
-    </>,
-    text: `Donec maximus tellus id convallis tristique. Duis quis ex non erat mattis hendrerit eu non orci. Integer dapibus elit quis tristique placerat. `
+    header: (
+      <>
+        Let's have a
+        <br />
+        Conversation.
+      </>
+    ),
+    text: (
+      <>
+        Unclear if somatic coaching and/or somatic bodywork is for you? That's OK, and let's have a conversation about it.
+        <br />
+        So far I’ve worked with movement organizers, entrepreneurs, executives, leaders, young professionals, members of the Navy, soon-to-be parents, current and former professional and olympic athletes, and many other identities people carry.
+        <br />I think somatic coaching and bodywork can benefit all, and at the same time is not for everyone. I am also not the coach/bodyworker for everyone. That's OK, too.
+      </>
+    ),
   },
-  contacts: [
-    'david@emergingsomatics.com',
-    '+1 213 308 7012',
-  ],
+  contacts: ['david@emergingsomatics.com', '+1 213 308 7012'],
   socials: [
     {
       icon: <MapsIcon />,
-      url: 'maps.google.com'
+      url: 'maps.google.com',
     },
     {
       icon: <TwitterIcon />,
-      url: 'twitter.com'
+      url: 'twitter.com',
     },
     {
       icon: <LinkedInIcon />,
-      url: 'linkedin.com'
-    }
+      url: 'linkedin.com',
+    },
   ],
 }
